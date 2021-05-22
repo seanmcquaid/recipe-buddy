1682947ecfe21ec0c58b4590f059bcdeaa0abb82
seanmcquaid <sean.mcquaid2@verizon.net>
2021-05-22 17:46:26 -0400

    added example actions test

--- src/store/loading/___tests___/actions.spec.js
+++ src/store/loading/___tests___/actions.spec.js
@@ -1,3 +1,19 @@
+import configureMockStore from '../../../testUtils/configureMockStore';
+import { loadingAction } from '../actions';
+
 describe('loading - actions', () => {
-  it('loadingAction', () => {});
+  it('loadingAction', () => {
+    const initialState = {
+      loading: {
+        isLoading: false,
+      },
+    };
+    const store = configureMockStore(initialState);
+
+    store.dispatch(loadingAction());
+
+    const actions = store.getActions();
+    const result = [{ type: 'LOADING' }];
+    expect(actions).toEqual(result);
+  });
 });
