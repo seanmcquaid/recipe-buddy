534395134c97515c0a039d0db9b98dfc2373e068
seanmcquaid <sean.mcquaid2@verizon.net>
2021-05-22 17:39:58 -0400

    added in reducers tests

--- src/store/loading/___tests___/reducer.spec.js
+++ src/store/loading/___tests___/reducer.spec.js
@@ -1,5 +1,23 @@
-describe('loading - reducer', () => {
-  it('LOADING', () => {});
+import reducer from '../reducer';
 
-  it('Default', () => {});
+describe('loading - reducer', () => {
+  it('LOADING', () => {
+    const state = {
+      isLoading: false,
+    };
+    const action = {
+      type: 'LOADING',
+    };
+    expect(reducer(state, action)).toEqual({ isLoading: true });
+  });
+
+  it('Default', () => {
+    const state = {
+      isLoading: true,
+    };
+    const action = {
+      type: 'DEFAULT',
+    };
+    expect(reducer(state, action)).toEqual({ isLoading: false });
+  });
 });
