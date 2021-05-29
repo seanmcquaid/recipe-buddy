10b4b2c41ec94988efd3a567d8e542d82d0ad63e
seanmcquaid <sean.mcquaid2@verizon.net>
2021-05-29 13:06:29 -0500

    added error message to login page

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,5 +1,28 @@
+import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+import { H1, P } from '../../components';
+import { userErrorMessageSelector } from '../../store/user/selectors';
+import LoginForm from './LoginForm';
+
 const Login = () => {
-  return null;
+  const userErrorMessage = useSelector(userErrorMessageSelector);
+  return (
+    <PageContainer>
+      <Header>
+        <H1>Login</H1>
+        <P>{userErrorMessage}</P>
+      </Header>
+      <Main>
+        <LoginForm />
+      </Main>
+    </PageContainer>
+  );
 };
+
+const PageContainer = styled.div``;
+
+const Header = styled.header``;
+
+const Main = styled.main``;
 
 export default Login;
