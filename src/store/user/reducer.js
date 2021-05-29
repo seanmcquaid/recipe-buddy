6883756345ed9b907caf6123fd42b0b31eb1f4df
seanmcquaid <sean.mcquaid2@verizon.net>
2021-05-29 12:51:23 -0500

    finished reducer for logging in and registering

--- src/store/user/reducer.js
+++ src/store/user/reducer.js
@@ -1,3 +1,10 @@
+import {
+  LOGIN_ERROR,
+  LOGIN_SUCCESS,
+  REGISTER_ERROR,
+  REGISTER_SUCCESS,
+} from './actionTypes';
+
 const initialState = {
   token: null,
   errorMessage: '',
@@ -5,6 +12,18 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case LOGIN_SUCCESS:
+    case REGISTER_SUCCESS:
+      return {
+        ...state,
+        token: action.payload.token,
+      };
+    case LOGIN_ERROR:
+    case REGISTER_ERROR:
+      return {
+        ...state,
+        errorMessage: action.payload.errorMessage,
+      };
     default:
       return {
         ...state,
