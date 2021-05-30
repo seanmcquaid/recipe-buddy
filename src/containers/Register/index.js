9008010c3332006da8af88afe21e7baad69574c9
seanmcquaid <sean.mcquaid2@verizon.net>
2021-05-30 14:51:03 -0500

    starting register page

---
+++ src/containers/Register/index.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+import { H1 } from '../../components';
+
+const Register = () => {
+  return (
+    <PageContainer>
+      <Header>
+        <H1>Register</H1>
+      </Header>
+      <Main></Main>
+    </PageContainer>
+  );
+};
+
+const PageContainer = styled.div``;
+
+const Header = styled.header``;
+
+const Main = styled.main``;
+
+export default Register;
