47073e60300d013ad2ecd70d2b38ab3219c56574
seanmcquaid <sean.mcquaid2@verizon.net>
2021-06-03 17:26:17 -0400

    recipes component completed

--- src/containers/SearchRecipes/Recipes.js
+++ src/containers/SearchRecipes/Recipes.js
@@ -1,10 +1,32 @@
 import propTypes from 'prop-types';
+import styled from 'styled-components';
+import { LoadingSpinner, P } from '../../components';
 import useSearchRecipesByIngredients from '../../hooks/useSearchRecipesByIngredients';
 
 const Recipes = ({ ingredients }) => {
   const { isLoading, errorMessage, recipes } =
     useSearchRecipesByIngredients(ingredients);
+
+  if (isLoading) {
+    return <LoadingSpinner isLoading={isLoading} />;
+  }
+
+  if (errorMessage) {
+    return <P>{errorMessage}</P>;
+  }
+
+  return (
+    <RecipesList>
+      {recipes.map((recipe) => (
+        <Recipe key={recipe.id}>{recipe.title}</Recipe>
+      ))}
+    </RecipesList>
+  );
 };
+
+const RecipesList = styled.ul``;
+
+const Recipe = styled.li``;
 
 Recipes.propTypes = {
   ingredients: propTypes.arrayOf(propTypes.string.isRequired).isRequired,
