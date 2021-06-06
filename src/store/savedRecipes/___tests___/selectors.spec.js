baf122de51e0e80bd052f15fc039efb342f9e301
seanmcquaid <sean.mcquaid2@verizon.net>
2021-06-06 18:48:57 -0400

    finished selectors tests

--- src/store/savedRecipes/___tests___/selectors.spec.js
+++ src/store/savedRecipes/___tests___/selectors.spec.js
@@ -1,1 +1,21 @@
-describe('savedRecipes - selectors', () => {});
+import { recipesSelector, savedRecipesSelector } from '../selectors';
+
+describe('savedRecipes - selectors', () => {
+  it('savedRecipesSelector', () => {
+    const state = {
+      savedRecipes: {
+        recipes: [],
+      },
+    };
+    expect(savedRecipesSelector(state)).toEqual({ recipes: [] });
+  });
+
+  it('recipesSelector', () => {
+    const state = {
+      savedRecipes: {
+        recipes: [],
+      },
+    };
+    expect(recipesSelector(state)).toEqual([]);
+  });
+});
