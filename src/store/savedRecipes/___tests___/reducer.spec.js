7f27d7f9a47247c8674eff1f9a2a3b43ab138cd6
seanmcquaid <sean.mcquaid2@verizon.net>
2021-06-06 21:56:52 -0400

    reducer test

--- src/store/savedRecipes/___tests___/reducer.spec.js
+++ src/store/savedRecipes/___tests___/reducer.spec.js
@@ -1,1 +1,18 @@
-describe('savedRecipes - reducer', () => {});
+import { ADD_RECIPE_SUCCESS } from '../actionTypes';
+import reducer from '../reducer';
+
+describe('savedRecipes - reducer', () => {
+  it('ADD_RECIPE_SUCCESS', () => {
+    const state = {
+      recipes: [],
+    };
+    const result = reducer(state, {
+      type: ADD_RECIPE_SUCCESS,
+      payload: { recipes: [{ id: 1, title: 'Parmesan' }] },
+    });
+
+    expect(result).toEqual({
+      recipes: [{ id: 1, title: 'Parmesan' }],
+    });
+  });
+});
