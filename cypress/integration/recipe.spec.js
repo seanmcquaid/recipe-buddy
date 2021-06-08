a3992aca14e6fbeefa2808e6a6bf85670a27b3d2
seanmcquaid <sean.mcquaid2@verizon.net>
2021-06-07 20:40:14 -0400

    finished cypress test for recipe info

--- cypress/integration/recipe.spec.js
+++ cypress/integration/recipe.spec.js
@@ -1,5 +1,23 @@
 describe('Recipe Page', () => {
-  beforeEach(() => {});
+  beforeEach(() => {
+    cy.intercept(
+      { method: 'GET', url: '*recipes/716429/information' },
+      { fixture: 'recipe.json' },
+    );
 
-  it('Adding recipe takes you back to recipe search page', () => {});
+    cy.visit('http://localhost:3000/recipe/716429');
+  });
+
+  it('Adding recipe takes you back to recipe search page', () => {
+    cy.intercept({ method: 'POST', url: '*recipes*' }, [
+      {
+        id: 716429,
+        title: 'Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs',
+      },
+    ]);
+
+    cy.get('[data-testid="Add RecipeButton"]').click();
+
+    cy.url().should('include', '/searchRecipes');
+  });
 });
