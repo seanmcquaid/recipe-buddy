5664b970dfde611ac8cf51647687b5ba0dc5f5a8
seanmcquaid <sean.mcquaid2@verizon.net>
2021-06-08 21:01:58 -0400

    added delete recipe action

--- src/store/savedRecipes/actions.js
+++ src/store/savedRecipes/actions.js
@@ -2,7 +2,7 @@ import recipesService from '../../services/recipesService';
 import { requestError } from '../error/actionCreators';
 import { loading } from '../loading/actionCreators';
 import { tokenSelector } from '../user/selectors';
-import { addRecipeSuccess } from './actionCreators';
+import { addRecipeSuccess, deleteRecipeSuccess } from './actionCreators';
 
 export const addRecipeAction = (id, title) => (dispatch, getState) => {
   dispatch(loading());
@@ -11,6 +11,21 @@ export const addRecipeAction = (id, title) => (dispatch, getState) => {
     .addRecipe(id, title, token)
     .then(({ data }) => {
       dispatch(addRecipeSuccess(data.recipes));
+      return Promise.resolve(data);
+    })
+    .catch((err) => {
+      dispatch(requestError(err.response.data.errorMessage));
+      return Promise.reject(err);
+    });
+};
+
+export const deleteRecipeAction = (id) => (dispatch, getState) => {
+  dispatch(loading());
+  const token = tokenSelector(getState());
+  return recipesService
+    .deleteRecipeById(id, token)
+    .then(({ data }) => {
+      dispatch(deleteRecipeSuccess(data.recipes));
       return Promise.resolve(data);
     })
     .catch((err) => {
