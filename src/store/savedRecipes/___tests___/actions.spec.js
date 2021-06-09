c6c6ee6ee64119fc1328d8dae995c3c0e73f3a7c
seanmcquaid <sean.mcquaid2@verizon.net>
2021-06-08 21:45:30 -0400

    added test for delete recipe action

--- src/store/savedRecipes/___tests___/actions.spec.js
+++ src/store/savedRecipes/___tests___/actions.spec.js
@@ -2,8 +2,8 @@ import recipesService from '../../../services/recipesService';
 import configureMockStore from '../../../testUtils/configureMockStore';
 import mockServiceSuccess from '../../../testUtils/mockServiceSuccess';
 import { LOADING } from '../../loading/actionTypes';
-import { addRecipeAction } from '../actions';
-import { ADD_RECIPE_SUCCESS } from '../actionTypes';
+import { addRecipeAction, deleteRecipeAction } from '../actions';
+import { ADD_RECIPE_SUCCESS, DELETE_RECIPE_SUCCESS } from '../actionTypes';
 
 describe('savedRecipes - actions', () => {
   it('addRecipeAction', () => {
@@ -27,6 +27,33 @@ describe('savedRecipes - actions', () => {
         {
           type: ADD_RECIPE_SUCCESS,
           payload: { recipes: [{ id: 1, title: 'Parmesan' }] },
+        },
+      ];
+      expect(actions).toEqual(result);
+    });
+  });
+
+  it('deleteRecipeAction', () => {
+    const initialState = {
+      user: {
+        token: 'token',
+      },
+      savedRecipes: {
+        recipes: [{ id: 1, title: 'Parmesan' }],
+      },
+    };
+    const store = configureMockStore(initialState);
+    mockServiceSuccess(recipesService, 'deleteRecipeById', {
+      data: { recipes: [] },
+    });
+
+    return store.dispatch(deleteRecipeAction(1)).then(() => {
+      const actions = store.getActions();
+      const result = [
+        { type: LOADING },
+        {
+          type: DELETE_RECIPE_SUCCESS,
+          payload: { recipes: [] },
         },
       ];
       expect(actions).toEqual(result);
