1498f75127c3dfcbe1acc082643acbe8c98ee394
seanmcquaid <sean.mcquaid2@verizon.net>
2021-06-08 21:03:14 -0400

    finished get recipes action

--- src/store/savedRecipes/actions.js
+++ src/store/savedRecipes/actions.js
@@ -2,7 +2,11 @@ import recipesService from '../../services/recipesService';
 import { requestError } from '../error/actionCreators';
 import { loading } from '../loading/actionCreators';
 import { tokenSelector } from '../user/selectors';
-import { addRecipeSuccess, deleteRecipeSuccess } from './actionCreators';
+import {
+  addRecipeSuccess,
+  deleteRecipeSuccess,
+  getRecipesSuccess,
+} from './actionCreators';
 
 export const addRecipeAction = (id, title) => (dispatch, getState) => {
   dispatch(loading());
@@ -26,6 +30,21 @@ export const deleteRecipeAction = (id) => (dispatch, getState) => {
     .deleteRecipeById(id, token)
     .then(({ data }) => {
       dispatch(deleteRecipeSuccess(data.recipes));
+      return Promise.resolve(data);
+    })
+    .catch((err) => {
+      dispatch(requestError(err.response.data.errorMessage));
+      return Promise.reject(err);
+    });
+};
+
+export const getRecipesAction = () => (dispatch, getState) => {
+  dispatch(loading());
+  const token = tokenSelector(getState());
+  return recipesService
+    .getRecipes(token)
+    .then(({ data }) => {
+      dispatch(getRecipesSuccess(data.recipes));
       return Promise.resolve(data);
     })
     .catch((err) => {
